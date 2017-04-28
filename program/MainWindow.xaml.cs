9ccdc94276273c57fb9a6c923d36f2a454a4052f
s0152942 <s0152942@ANT027>
2017-04-28 08:58:02 +0200

    Add project files.

---
+++ program/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static Task<string> znajdz_r()
+        {
+            return Task.Run(() =>
+            {
+
+
+                return "napis";
+
+
+            }
+
+                );
+        }
+
+        private async void button_r_Click(object sender, RoutedEventArgs e)
+        {
+            string wynik = await znajdz_r();
+            textBox_r.Text = wynik;
+        }
+        static Task<string> znajdz_s()
+        {
+            return Task.Run(() => 
+            {
+
+
+                return "napis";
+
+
+            }
+                
+                );
+        }
+        private async void button_s_Click(object sender, RoutedEventArgs e)
+        {
+            string wynik = await znajdz_s();
+            textBox_s.Text = wynik;
+        }
+    }
+}
